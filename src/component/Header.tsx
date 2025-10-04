4450392a95deeefaacef522c8fd5778323536ccd
7his15CYS <127721339+7his15CYS@users.noreply.github.com>
2025-10-05 01:17:55 +0800

    Update Header.tsx

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -4,8 +4,8 @@ export function Header(){
     const navigate = useNavigate()
     return (
         <div className="w-100 pt-5 pb-4 rounded">
-            <p className="fs-2 fw-bold" onClick={() => navigate("/")} style={{cursor: "pointer"}}>資訊安全實驗室</p>
-            <h6 className="m-0" ><small style={{color: "#ffffff99"}}>國立臺北科技大學 資訊工程學系</small></h6>
+            <p className="fs-2 fw-bold" onClick={() => navigate("/")} style={{cursor: "pointer"}}>is1ab CLUB</p>
+            <h6 className="m-0" ><small style={{color: "#ffffff99"}}>國立臺北科技大學</small></h6>
         </div>
     )
 }
