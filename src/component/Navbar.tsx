399e4114f2cb583b5df29c23fb586094c7a1bdc7
7his15CYS <127721339+7his15CYS@users.noreply.github.com>
2025-10-05 01:28:23 +0800

    Update Navbar.tsx

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -10,57 +10,12 @@ export function Navbar() {
       children: [
         {
           key: "member",
-          label: "實驗室成員 Members",
+          label: "成員 Members",
           onClick: () => navigate("/Member"),
         },
-        {
-          key: "alumni",
-          label: "畢業成員 Alumni",
-          onClick: () => navigate("/Alumni"),
-        },
-        {
-          key: "rule",
-          label: "實驗室規則 Rules",
-          onClick: () => navigate("/Rules"),
-        },
-        {
-          key: "partner",
-          label: "合作夥伴 Partners",
-          onClick: () => navigate("/Partner"),
-        },
       ],
     },
-    {
-      key: "about prof",
-      label: "指導教授 About Dr. Sun",
-      children: [
-        {
-          key: "Profile",
-          label: "簡介 Profile",
-          onClick: () => navigate("/Profile"),
-        },
-        {
-          key: "publication",
-          label: "著作 Publication",
-          onClick: () => navigate("/Publication"),
-        },
-        {
-          key: "experience",
-          label: "經歷 Experience",
-          onClick: () => navigate("/Experience"),
-        },
-        {
-          key: "Courses",
-          label: "課程 Courses",
-          onClick: () => navigate("/Course"),
-        },
-        {
-          key: "records",
-          label: "紀錄 Records",
-          onClick: () => navigate("/Record"),
-        },
-      ],
-    },
+    
     {
       key: "news",
       label: "消息 News",
@@ -70,16 +25,6 @@ export function Navbar() {
       key: "activities",
       label: "活動 Activities",
       onClick: () => navigate("/Activities"),
-    },
-    {
-      key: "calendar",
-      label: "實驗室活動 Calendar",
-      onClick: () => navigate("/Calendar"),
-    },
-    {
-      key: "club",
-      label: "is1ab Club",
-      onClick: () => navigate("/Clubs"),
     },
   ];
   return (
