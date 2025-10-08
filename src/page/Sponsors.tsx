5922a5cddb0d2c095a2ac9d27bd0fc2a1808e0ed
7his15CYS <127721339+7his15CYS@users.noreply.github.com>
2025-10-08 09:01:48 +0800

    Update Sponsors.tsx

--- src/page/Sponsors.tsx
+++ src/page/Sponsors.tsx
@@ -8,9 +8,6 @@ export function Sponsors(){
                 {datas.map(data => {
                     return (
                         <tr>
-                            <td className="w-25 border border-1 p-3">
-                                <p>{data.time}</p>
-                            </td>
                             <td className="w-50 border border-1 p-3">
                                 <p>{data.title}</p>
                             </td>
