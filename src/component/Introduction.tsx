ce2429f71dfdddff03bb6dc96d1cc48d432299dc
7his15CYS <127721339+7his15CYS@users.noreply.github.com>
2025-10-09 08:49:08 +0800

    Update Introduction.tsx

--- src/component/Introduction.tsx
+++ src/component/Introduction.tsx
@@ -8,6 +8,7 @@ export function Introduction(){
         <div className="p-3 py-4 introduction">
             <h4 className="text-center fw-bold mb-3">簡介</h4>
             <p> <span style={{fontWeight: 800}}>is1ab club</span> 是由社長 Adb2 和其他 8 位夥伴（dkri3c1、lywslc、FromKeyToEnd、youngleetw、c1ydehhx、Guan4Tou2、VV1NN、與 WIFI）於 2025 年 10 月共同創立的「北科大學生社團」。旨於成立資安 CTF 戰隊，並活躍於國內（外）各式各樣的資安競賽。同時，is1ab club 預計：1) 每年協助北科大/北聯大舉辦資安 CTF 大型賽事與舉辦校內資安相關活動、2) 暑假期間提供國內學校單位相關資安宣導與技術協助等服務、3) 每月定期舉辦讀書會分享彼此的研究與技術成果、並且我們4) 不定期與國內著名資安企業交流。</p>
+            <br></br>
             <p> 我們歡迎國內不限年齡的夥伴們加入一起學習。</p>
         </div>
     )
