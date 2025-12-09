ec88a219dc5f29d864961cd05a2b4d298c0f43d2
xadb2 <adb234x@gmail.com>
2025-12-09 11:28:50 +0800

    Update is1ab club to 北科資安社.

--- src/component/Alert.tsx
+++ src/component/Alert.tsx
@@ -1,7 +1,7 @@
 export function Alert(){
     return (
         <div className="p-3 bg-black border border-white border-start-0 border-end-0 border-2 text-center">
-            <p className="fw-bold m-0" style={{color: "yellow"}}>祝賀 is1ab club 成立</p>
+            <p className="fw-bold m-0" style={{color: "yellow"}}>北科資安社成立</p>
         </div>
     )
 }
